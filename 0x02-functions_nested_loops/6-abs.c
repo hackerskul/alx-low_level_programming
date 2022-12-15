dc95d02ccf92417c563097ec069e5dca82fd3562
hackerskul <hackerskull254@gmail.com>
2022-12-15 03:55:29 -0500

    correction

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abs value of an int
+ * @n: the int to be checked
+ *
+ * Return: the absolute value of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
