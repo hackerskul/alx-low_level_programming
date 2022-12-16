7e40eb1c1eeb44a41dad8520d2f389b880da8c93
hackerskul <hackerskull254@gmail.com>
2022-12-16 09:48:19 -0500

    correction

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * print_line - main entry point
- * @n: the main variable
+ * print_line-draws a line
+ * @n:the param
+ * Return:void
  */
 void print_line(int n)
 {
-	int i;
-
-	for  (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		_putchar('-');
+		_putchar('_');
 	}
 	_putchar('\n');
 }
