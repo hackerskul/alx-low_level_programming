afe6a7e9c7eacbd140cb97ed0f17cc8867d57789
hackerskul <hackerskull254@gmail.com>
2023-01-06 03:33:16 -0500

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,6 +3,7 @@
  *_pow_recursion - raises a number to power
  *@x: an integer that will carry the number to be raised to power
  *@y: an integer telling how much to be raised to power
+ *Return: returns what is writen there
  *return: returns -1 if int y is less than 0
  */
 int _pow_recursion(int x, int y)
