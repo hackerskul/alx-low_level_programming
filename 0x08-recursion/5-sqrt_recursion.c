afe9be57e86f6a79563d03a5036c1793b2e29be6
hackerskul <hackerskull254@gmail.com>
2023-01-06 04:12:57 -0500

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *sqrt_recursion - calculates the square root
+ *@n: integer for the nmbr
+ *@guess: also an int
+ *Return: returns
+ *return: also returns
+ */
+int sqrt_recursion(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	return (sqrt_recursion(n, (n / guess + guess) / 2));
+}
