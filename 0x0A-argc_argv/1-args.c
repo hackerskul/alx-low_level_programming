ab84c56c88be4670ec64fe9832045bc563121d17
hackerskul <hackerskull254@gmail.com>
2023-01-12 04:44:03 -0500

    argc name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of command-line arguments passed to the program
+ * @argc: argument count
+ * @argv: pointer to string argc
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d", argc -1);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -15,5 +15,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char);
 int main(int argc __attribute__((unused)), char *argv[]);
-
+int main(int argc, char *argv[] __attribute__((unused)));
 #endif
