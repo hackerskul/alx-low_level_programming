4d929af9087f127f0dc0f581d2b3e53f686882fb
hackerskul <hackerskull254@gmail.com>
2023-01-12 03:42:13 -0500

    argc name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - returns the name of the argument
+ * argc - argument count
+ * argv - pointer to string argc
+ * return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
