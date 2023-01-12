07d327219a7fc54f5bf2a4926ce3eb564c2d54b2
hackerskul <hackerskull254@gmail.com>
2023-01-12 04:57:28 -0500

    argc count

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - function to print name
+ * @argc: argc parameter
+ * @argv: an array
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, int *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
