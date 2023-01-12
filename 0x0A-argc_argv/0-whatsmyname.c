c7b6e14493a250bc30c70d82aac3e2eca9b93197
hackerskul <hackerskull254@gmail.com>
2023-01-12 04:26:28 -0500

    argc name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,12 +10,13 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-    int i = 0;
-    while (argv[0][i])
-    {
-        _putchar(argv[0][i]);
-        i++;
-    }
-    _putchar('\n');
-    return (0);
+	int i = 0;
+
+	while (argv[0][i])
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
