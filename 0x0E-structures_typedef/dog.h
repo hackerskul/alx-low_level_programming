bd6f00a931c2b82a0c0692ad958c4442c309fd4b
hackerskul <hackerskull254@gmail.com>
2023-01-30 03:23:25 -0500

    c structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
-struct dog 
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+/**
+ * struct dog - a structure to represent a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
 };
+
+
+#endif
