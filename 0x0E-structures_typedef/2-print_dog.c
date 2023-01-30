171cf0e7e1adbe907e25d5af43aee05266c64473
hackerskul <hackerskull254@gmail.com>
2023-01-30 04:33:58 -0500

    c structures quiz 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,37 @@
+#include <stdlib>
+#include "dog.h"
+/**
+ * print_dog - print the members of a dog structure instance
+ * @d: pointer to the dog structure instance that needs to be printed
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == NULL)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %s\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s", d->owner);
+		}
+	}
+}
