0ea0c36e0855c33b3ddefdd937b7b668ef649c57
hackerskul <hackerskull254@gmail.com>
2023-02-19 07:42:39 -0500

    tst

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "holberton.h"
 
 /**
- *binary_to_uint - function to count binary nmbrs
- *@b: pointer to the string of binary
- *Return: returns
+ *_pow - returns the value of x raised to the y
+ *
+ *@x: base number
+ *@y: exponent
+ *
+ *Return: @x to the @y, if @y < 0 return -1
  */
+int _pow(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow(x, y - 1));
+}
 
+/**
+ *binary_to_uint - converts a binary number to an unsigned int
+ *@b: string containing the binary number
+ *Return: Return the converted number, or 0 if the string has a char other than
+ *0 or 1 or if @b is NULL
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int nmbr = 0;
-	int power = 0;
-	int i;
+	unsigned int num = 0;
+	int i, j;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
-	for (i = strlen(b); i >= 0; i--)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			nmbr += (b[i] - '0') * (1 << power);
-			power++;
-		}
-		else
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
 	}
-	return (nmbr);
+	i--;
+	for (j = 0; i >= 0; i--, j++)
+	{
+		if (b[i] == '1')
+			num += _pow(2, j);
+	}
+	return (num);
 }
