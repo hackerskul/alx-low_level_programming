9e0cfd5040a6abbb54a0a01223aae7ef3c696481
hackerskul <hackerskull254@gmail.com>
2023-04-18 07:42:45 -0400

    added a prototype file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+/**
+ *print_dlistint - is a func for printing total nodes
+ *@h: is a pointer to the first node of the list
+ *Return: the total nodes
+ */
 size_t print_dlistint(const dlistint *h)
 {
 	const *current = h;
-
 	size_t nmbr = 0;
 
 	while (current != Null)
 	{
 		current = current->next;
-	        nmbr++;
+		nmbr++;
 	}
 
 	return (nmbr);
