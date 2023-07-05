f3ffbe21155f3bc9cc60defa412c6bd7fd17921a
hackerskul <hackerskull254@gmail.com>
2023-07-05 05:47:25 -0400

    function description

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
+/*
+ * _print_rev_recursion - is the reverse function
+ * @s: is a pointer to a string to be reversed
+ */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s != 0)
