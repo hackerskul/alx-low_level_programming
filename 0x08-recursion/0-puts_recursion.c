1d36c54bc412b58a4655bbbf22fc6a0492b8a50c
hackerskul <hackerskull254@gmail.com>
2023-07-05 05:35:01 -0400

    len of elements in a linked list

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ *_puts_recursion - prints a string and adds a newline using recursion
+ *
+ *@s: pointer that has the address of the string to be printed
+ *
+ *Return: nothing
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s == 0)
