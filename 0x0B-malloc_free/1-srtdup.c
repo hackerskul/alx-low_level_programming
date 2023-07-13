2f657ce8fe39fd685ba73798f147239ac1b52326
hackerskul <hackerskull254@gmail.com>
2023-07-13 08:42:23 -0400

    returning a pointer to a newly allocated strng

---
+++ 0x0B-malloc_free/1-srtdup.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ *_strdup -  returns a pointer to a newly allocated space in memory, which
+ *contains a copy of the string given as a parameter
+ *
+ *@str: the string to be copied
+ *
+ *Return: a pointer to the array or NULL if the process fails
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (ptr == 0)
+		return (0);
+
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = str[i];
+	}
+
+	return (ptr);
+}
