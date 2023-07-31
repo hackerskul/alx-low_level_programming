1f2c37db920c93f9d57a68883e7cd5ae9f6356c0
hackerskul <hackerskull254@gmail.com>
2023-07-31 15:25:30 -0400

    correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,9 @@ listint_t *add_node(listint_t **head, const int n)
 {
 	listint_t *new_node;
 	
-	new_node = (listint_t *)malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return NULL;
 
 	new_node->n = n;
 	new_node->next = *head;
