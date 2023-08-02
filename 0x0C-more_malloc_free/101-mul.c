f470123c5e55502b40708ef3e495521b9b5e9f51
hackerskul <hackerskull254@gmail.com>
2023-08-02 08:27:33 -0400

    correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,39 +2,46 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int is_valid_number(char *str) {
-    while (*str) {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
-    return 1;
+int is_valid_number(char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return 0;
+		str++;
+	}
+	return 1;
 }
 
-int multiply_numbers(int num1, int num2) {
-    return num1 * num2;
+int multiply_numbers(int num1, int num2)
+{
+	return num1 * num2;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return 98;
-    }
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 98;
+	}
 
-    char *num1_str = argv[1];
-    char *num2_str = argv[2];
+	char *num1_str = argv[1];
+	char *num2_str = argv[2];
 
-    if (!is_valid_number(num1_str) || !is_valid_number(num2_str)) {
-        printf("Error\n");
-        return 98;
-    }
+	if (!is_valid_number(num1_str) || !is_valid_number(num2_str))
+	{
+		printf("Error\n");
+		return 98;
+	}
 
-    int num1 = atoi(num1_str);
-    int num2 = atoi(num2_str);
+	int num1 = atoi(num1_str);
+	int num2 = atoi(num2_str);
 
-    int result = multiply_numbers(num1, num2);
+	int result = multiply_numbers(num1, num2);
 
-    printf("%d\n", result);
+	printf("%d\n", result);
 
-    return 0;
+	return 0;
 }
+
