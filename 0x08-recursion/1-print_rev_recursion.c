bf92b128ffc33982183525a7f3a2b217fdb9abbb
hackerskul <hackerskull254@gmail.com>
2023-08-30 04:45:11 -0400

    description in the rev function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ *_print_rev_recursion - prints a string in reverse and adds a newline using
+ *recursion
+ *
+ *@s: pointer that has the address of the string to be printed
+ *
+ *Return: nothing
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
