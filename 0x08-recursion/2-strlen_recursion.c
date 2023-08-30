296815999c7dd3ba9b4760b68c52dc3912940a1e
hackerskul <hackerskull254@gmail.com>
2023-08-30 04:50:20 -0400

    counting chars in a string using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - prints the length of a string using recursion
+ *
+ *@s: pointer that has the address of the string
+ *
+ *Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	if (s == '\n')
+	{
+		return();
+	}
+	else
+	{
+		_strlen_recursion(s + 1);
+		n = n + 1;
+	}
+	return (n);
+
+}
